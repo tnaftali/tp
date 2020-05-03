df0afd40010add5ce68ea4fd36a4dd334f7123c7
Tobias Naftali <tobias.naftali@gmail.com>
2020-05-03 18:19:32 -0300

    ingresar lista manualmente, README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Algoritmos y Estructuras de Datos II
+## TP 1
+### Profesor: Sergio Amitrano
+### Lenguaje: Python
+### Integrantes del grupo:
+- María Pía Pepe
+- Tobías Naftali
+- Rodrigo Federico Quiroga
+
+#### Para correr el programa:
+1. Tener instalado Python (3+)
+2. En una consola, correr `python3 permutaciones.py`
+3. Ingresar números deseados para la lista, separados por coma
--- permutaciones.py
+++ permutaciones.py
@@ -1,32 +1,32 @@
-## TP1 Algoritmos y Estructuras de Datos II
-## Integrantes:
-## Maria Pia Pepe
-## Tobias Naftali
-## Rodrigo Federico Quiroga
-def permutaciones(lst):
-    if (len(lst) == 0):
-        ## evaluo si la lista esta vacia, en cuyo caso devuelve una lista vacia
-        return []
-    elif (len(lst) == 1):
-        ## si la lista tiene 1 solo elemento, retorno ese elemento de la lista
-        return [lst]
-    else:
-        ## creo una nueva lista con las nuevas permutaciones
-        l=[]
-        ## recorro la lista con un for
-        ## me quedo con el primer elemento de la lista para luego hacer las
-        ## combinaciones con todos los elementos subsiguientes
-        ## realizo la recursion con los elementos subsiguientes buscando las permutaciones
-        ## guardo todos los resultados en la lista final, la cual retorno
-        for i in range(len(lst)):
-            eInicial=lst[i]
-            eSubsiguientes = lst[:i] + lst[i+1:]
-            for p in permutaciones(eSubsiguientes):
-                l.append([eInicial]+p)
-        return l
-            
-            
-lista = [6,2,3]
+import sys
+
+def permutations(list):
+  if len(list) == 0:
+    ## evaluo si la lista esta vacia, en cuyo caso devuelvo una lista vacia
+    return []
+  elif len(list) == 1:
+    ## si la lista tiene 1 solo elemento, retorno ese elemento de la lista
+    return [list]
+  else:
+    ## creo una nueva lista con las nuevas permutaciones
+    perm_list = []
+    ## recorro la lista con un for
+    ## me quedo con el primer elemento de la lista para luego hacer las
+    ## combinaciones con todos los elementos subsiguientes
+    ## realizo la recursion con los elementos subsiguientes buscando las permutaciones
+    ## guardo todos los resultados en la lista final, la cual retorno
+    for i in range(len(list)):
+      current = list[i]
+      next = list[:i] + list[i+1:]
+      for p in permutations(next):
+        perm_list.append([current]+p)
+    return perm_list
+
+
 ## lista de el ementos que se va a permutar
-for p in permutaciones(lista):
-    print(p)
+list = []
+inserted_items = input("Ingresar números separados por coma\n").split(",")
+for item in inserted_items:
+  list.append(int(item))
+for p in permutations(list):
+  print(p)
